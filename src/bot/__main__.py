1f857ea9df78cb25667d1bf3a57da2a4c6140468
ddjerqq <ddjerqq@gmail.com>
2024-01-14 22:03:54 +1300

    added sync command to owner limit

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -1,9 +1,52 @@
 from __future__ import annotations
 import os
+from typing import Optional, Literal
+
+import discord
+from discord.ext import commands
+from bot import DiscordBot, logger
 from dotenv import load_dotenv
-from bot import DiscordBot
 
 load_dotenv()
 
 bot = DiscordBot()
+
+
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(
+        ctx: commands.Context,
+        guilds: commands.Greedy[discord.Object],
+        spec: Optional[Literal["~", "*", "^"]] = None
+) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+            ret += 1
+        except discord.HTTPException:
+            pass
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+
 bot.run(os.environ["BOT__TOKEN"])
